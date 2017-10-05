a66a8f463e0da5b5ee2c2a67d8f7966284740e67
Tim <tim.garret@inseec-france.com>
2017-10-05 19:52:44 +0200

    version page google incomplète

---
+++ google.js
@@ -0,0 +1,9 @@
+let boutonCentre = document.getElementById('boutoncentre');
+let milieu = document.getElementById('milieu');
+
+boutonCentre.onclick = function(){
+    this.style.visibility = "hidden";
+    milieu.style.display = "block";
+  }
+setTimeout(function() { alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+}, 10000);
